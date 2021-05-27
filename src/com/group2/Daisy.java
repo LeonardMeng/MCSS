5869a5fd5b256c01f18043074ebcb9a3ef94ec5a
Wiger <31722033+Wiger123@users.noreply.github.com>
2021-05-27 14:33:39 +0800

    Add comments

--- src/com/group2/Daisy.java
+++ src/com/group2/Daisy.java
@@ -4,32 +4,39 @@ package com.group2;
  * Daisy
  */
 public class Daisy {
+  // Private variable
   private int age;
   private DaisyType daisyType;
   private double albedoBlack = Parameters.BLACK_ALBEDO;
   private double albedoWhite = Parameters.WHITE_ALBEDO;
-
+  
+  // Constructor
   public Daisy(DaisyType daisyType) {
     this.daisyType = daisyType;
   }
-
+  
+  // Constructor
   public Daisy(int age, DaisyType daisyType) {
     this.age = age;
     this.daisyType = daisyType;
   }
-
+  
+  // Return the age
   public int getAge() {
     return age;
   }
 
+  // Set the age
   public void setAge(int age) {
     this.age = age;
   }
 
+  // Return the type of daisy
   public DaisyType getDaisyType() {
     return daisyType;
   }
 
+  // Return the albedo
   public double getAlbedoRate(){
     if(daisyType == DaisyType.BLACK)
       return albedoBlack;
@@ -38,7 +45,8 @@ public class Daisy {
     else
       return 0;
   }
-
+  
+  // Set the type of daisy
   public void setDaisyType(DaisyType daisyType) {
     this.daisyType = daisyType;
   }
